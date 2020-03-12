2baa6f29fc3d34d264f09758c6b5af0218bbcb99
Emil Nilsson <emil.nilsson@protonmail.com>
2020-03-12 22:18:50 +0100

    Set min lein version to 2.9.1

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :min-lein-version "2.9.1"
   :dependencies [[clojure.jdbc/clojure.jdbc-c3p0 "0.3.1"]
                  [compojure "1.6.1"]
                  [environ "1.1.0"]

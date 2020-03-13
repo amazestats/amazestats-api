8a557b58bd04f5b9d8227a2a576de45f36dc61fd
Emil Nilsson <emil.nilsson@protonmail.com>
2020-03-13 16:33:40 +0100

    Add url in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject amazestats "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://amazestats-api.herokuapp.com"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :min-lein-version "2.9.1"

e4881139145d12c270064fce23e103fc4e15b72b
Emil Nilsson <emil.nilsson@protonmail.com>
2020-08-06 00:18:23 +0200

    Fix broken tests

--- test/amazestats/handlers/match_test.clj
+++ test/amazestats/handlers/match_test.clj
@@ -66,7 +66,7 @@
                   db/get-match-by-id (fn [_] nil)
                   competition-db/competition-admin? (fn [_c _u] true)]
       (is (= (update-match-referee "20" {:identity {:user-id 5}
-                                         :body {:referee "17"}})
+                                         :body {:id "17"}})
              {:status 404
               :headers {}
               :body {:message "The match does not exist."}}))))
@@ -76,7 +76,7 @@
                   db/get-match-by-id (fn [_] {:id 20})
                   db/get-competition-id-for-match (fn [_] 17)]
       (is (= (update-match-referee "20" {:identity {:user-id 5}
-                                         :body {:referee "17"}})
+                                         :body {:id "17"}})
              {:status 403
               :headers {}
               :body
@@ -89,18 +89,18 @@
                   db/get-match-by-id (fn [_] {:id 20})
                   competition-db/competition-admin? (fn [_c _u] true)]
       (is (= (update-match-referee "20" {:identity {:user-id 6}
-                                         :body {:referee "17"}})
+                                         :body {:id "17"}})
              {:status 400
               :headers {}
               :body {:message "Could not appoint team as referee."}}))))
 
   (testing "200 The updated resource is returned"
-    (with-redefs [db/set-match-referee (fn [_ ref] {:referee ref})
+    (with-redefs [db/set-match-referee (fn [_ ref] {:id ref})
                   db/get-match-by-id (fn [_] {:id 20})
                   db/get-competition-id-for-match (fn [_] 17)
                   competition-db/competition-admin? (fn [_c _u] true)]
       (is (= (update-match-referee "20" {:identity {:user-id 5}
-                                         :body {:referee "17"}})
+                                         :body {:id "17"}})
              {:status 200
               :headers {}
-              :body {:referee 17}})))))
+              :body {:id 17}})))))

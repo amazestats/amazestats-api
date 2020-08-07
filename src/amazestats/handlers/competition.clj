55b7fdcd5e55dbe2357e599ca225516d37ef2708
Emil Nilsson <emil.nilsson@protonmail.com>
2020-08-07 17:10:27 +0200

    Expose endpoint for getting competition admins
    Further additions to the same resource will be the ability to post and
    delete admins for competitions.

--- src/amazestats/handlers/competition.clj
+++ src/amazestats/handlers/competition.clj
@@ -1,6 +1,7 @@
 (ns amazestats.handlers.competition
   (:require [amazestats.database.competition :as db]
-            [amazestats.util.response :refer [internal-error not-found ok]]))
+            [amazestats.util.response :refer [internal-error not-found ok]]
+            [clojure.set :as set]))
 
 (defn get-all-competitions
   "Create a response map containing all competitions.
@@ -21,3 +22,17 @@
     (if (nil? competition)
       (not-found "Competition does not exist.")
       (ok {:competition competition}))))
+
+(defn get-competition-admins
+  "Create a response with all admins for a competition with given `id`.
+  Returns a 404 if the competition does not exist, otherwise a map containing
+  the `id` and `alias` for all admins in the competition."
+  [competition-id]
+  (let [competition-id (Integer. competition-id)
+        admins (db/get-competition-admins competition-id)]
+    (if (and (empty? admins) (nil? (db/get-competition-by-id competition-id)))
+      (not-found "Competition does not exist.")
+      (let [admins (map (fn [admin]
+                          (set/rename-keys admin {:admin :id}))
+                        admins)]
+        (ok {:admins admins})))))

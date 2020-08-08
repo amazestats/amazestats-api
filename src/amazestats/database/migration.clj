51caa25b8c2404260084b7fcf409d43a60845523
Emil Nilsson <emil.nilsson@protonmail.com>
2020-08-08 02:56:43 +0200

    Mark all migrations as complete on init
    New initializations of the database should not require any of the
    current migrations to be ran, as the `init.sql` should be up-to-date
    with all the changes from the migration scripts. Marking these as
    completed will stop any of them to be executed the during the next
    launch.
    
    This allows the migration scripts to have reduced logic regarding when
    they should be executed.

--- src/amazestats/database/migration.clj
+++ src/amazestats/database/migration.clj
@@ -1,6 +1,9 @@
 (ns amazestats.database.migration
   (:require [amazestats.database.core :refer [db-spec]]
-            [migratus.core :as migratus]))
+            [clojure.tools.logging :as log]
+            [migratus.core :as migratus]
+            [migratus.database :as migratus-db]
+            [migratus.protocols :as proto]))
 
 (def config {:store                 :database
              :migration-dir         "sql/migrations/"
@@ -8,11 +11,49 @@
              :migration-table-name  "migrations"
              :db                    db-spec})
 
-(defn init
+(defn mark-complete
+  "Mark a `migration` as completed."
+  [migration]
+  (log/info "Marking:" (proto/id migration))
+  (let [db (:db config)
+        table-name (migratus-db/migration-table-name config)
+        migration-name (:name migration)
+        migration-id (proto/id migration)]
+    (migratus-db/mark-complete db table-name migration-name migration-id)))
+
+(defn mark-all-migrations-complete
+  "Marks all migrations as completed without running them."
   []
-  (migratus/init config))
+  (let [store (proto/make-store config)]
+    (try
+      (proto/connect store)
+      (let [migrations (->> (migratus/uncompleted-migrations config store)
+                            (sort-by proto/id))]
+        (when (seq migrations)
+          (loop [[migration & more] migrations]
+            (when migration
+              (mark-complete migration)
+              (recur more)))))
+
+      (catch Exception e
+        (log/error "Error occurred during migration marking:" e))
+      (finally (proto/disconnect store)))))
+
+(defn create-migration-table
+  "Create the migration table."
+  []
+  (proto/connect (proto/make-store config)))
+
+(defn init
+  "Initializes the database and marks all migrations as completed."
+  []
+  (migratus/init config)
+  (create-migration-table)
+  (log/info "Marking migrations as complete during initialization.")
+  (mark-all-migrations-complete))
 
 (defn migrate
+  "Run uncompleted migrations."
   []
   (migratus/migrate config))
 
@@ -21,6 +62,8 @@
   (migratus/rollback config))
 
 (defn create
+  "Create new migration.
+  The migration will be created in the migration directory."
   []
   (migratus/create config))
               
